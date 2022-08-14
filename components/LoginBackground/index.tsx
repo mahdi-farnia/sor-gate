2ef07b520f4fdc79db8e18696172bbe1dd145fd1
mahdi-farnia <mohammadmahdi1383@outlook.com>
2022-08-14 22:09:41 +0430

    login has dimmed and blurry bg

---
+++ components/LoginBackground/index.tsx
@@ -0,0 +1,19 @@
+import styles from './LoginBackground.module.scss';
+import Image from 'next/image';
+import backgroundImage from '../../assets/login-bg.jpg';
+
+const LoginBackground: React.FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <Image
+        layout="fill"
+        draggable={!1}
+        objectFit="cover"
+        src={backgroundImage}
+        alt="Login background"
+      />
+    </div>
+  );
+};
+
+export default LoginBackground;
