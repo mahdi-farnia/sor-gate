a9172e1d30f7ca7286b641dd9f263b246d69700f
mahdi-farnia <mohammadmahdi1383@outlook.com>
2022-08-14 22:13:06 +0430

    rename components folder to ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 
-import LoginBackground from '../components/LoginBackground';
+import LoginBackground from '../ui/LoginBackground';
 
 const Home: NextPage = () => {
   return (
