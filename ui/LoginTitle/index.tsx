4dcaa82d717690167f6cb46be03d669d36a7bb15
mahdi-farnia <mohammadmahdi1383@outlook.com>
2022-08-15 16:57:07 +0430

    SoR text have animation, but need enhancments

--- ui/LoginTitle/index.tsx
+++ ui/LoginTitle/index.tsx
@@ -1,10 +1,15 @@
 import styles from './styles.module.scss';
+import src from '../../assets/sor-font-logo-animation.gif';
+import Image from 'next/image';
 
 const LoginTitle: React.FC = () => {
   return (
     <header className={`${styles.header} no-select`}>
       <div className={styles.wrapper}>
-        <h1>Welcome SoR</h1>
+        <h1>
+          Welcome
+          <Image src={src} alt="JJ" layout="intrinsic" />
+        </h1>
         <p className="font-secondary">
           <span className={styles.description}>Enter your password to unlock</span>
         </p>
