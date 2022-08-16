0c1c193d0b85709c43a74868575ac5eea8324e39
mahdi-farnia <mohammadmahdi1383@outlook.com>
2022-08-16 20:01:13 +0430

    index page ui completed

--- ui/LoginTitle/index.tsx
+++ ui/LoginTitle/index.tsx
@@ -1,16 +1,43 @@
 import styles from './styles.module.scss';
-import src from '../../assets/sor-font-logo-animation.gif';
 import Image from 'next/image';
+import AnimatedTitle from '../../assets/animated-title.gif';
+import { useCallback, useEffect, useRef } from 'react';
 
 const LoginTitle: React.FC = () => {
+  const headerRef = useRef<HTMLHeadingElement>(null);
+  const paraRef = useRef<HTMLParagraphElement>(null);
+  const imageLoad = useCallback(() => {
+    const { current: header } = headerRef;
+
+    if (!header) return;
+
+    header.classList.add('loaded');
+  }, [headerRef]);
+
+  useEffect(() => {
+    const { current: para } = paraRef;
+    if (!para) return;
+
+    const animEnd = () => para.classList.add('anim-ended');
+
+    para.addEventListener('animationend', animEnd, { once: !0 });
+    return () => para.removeEventListener('animationend', animEnd);
+  }, [paraRef]);
+
   return (
-    <header className={`${styles.header} no-select`}>
+    <header className={`${styles.header} no-select`} ref={headerRef}>
       <div className={styles.wrapper}>
         <h1>
-          Welcome
-          <Image src={src} alt="JJ" layout="intrinsic" />
+          <span className={styles.hiddenTitle}>Welcome SoR</span>
+          <Image
+            src={AnimatedTitle}
+            alt="SoR Animated Title"
+            layout="fill"
+            unselectable="on"
+            onLoad={imageLoad}
+          />
         </h1>
-        <p className="font-secondary">
+        <p className="font-secondary" ref={paraRef}>
           <span className={styles.description}>Enter your password to unlock</span>
         </p>
       </div>
